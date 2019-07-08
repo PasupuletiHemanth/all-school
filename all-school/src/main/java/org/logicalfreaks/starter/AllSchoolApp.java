c91fff2d82d6bb1a9a7b67a62a179e13efa4c0ff
hemanthp1 <pasupuleti.hemanth@gmail.com>
2019-07-08 22:35:15 +0530

    test check in - removed spaces

--- all-school/src/main/java/org/logicalfreaks/starter/AllSchoolApp.java
+++ all-school/src/main/java/org/logicalfreaks/starter/AllSchoolApp.java
@@ -7,10 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AllSchoolApp {
 
 	public static void main(String[] args) {
-		
     SpringApplication.run(AllSchoolApp.class, args);
-    
-
 	}
 
 }
